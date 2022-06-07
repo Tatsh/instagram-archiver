843ff75d94c9ea1b2b22746b42b1c6485ce466ef
Andrew Udvare <audvare@gmail.com>
2022-06-07 16:49:16 -0400

    .stubs/yt_dlp/extractor/instagram: remove unused method

--- .stubs/yt_dlp/extractor/instagram.pyi
+++ .stubs/yt_dlp/extractor/instagram.pyi
@@ -1,8 +1,5 @@
-from typing import Any, Mapping, Sequence
 from yt_dlp.extractor.common import InfoExtractor
 
 
 class InstagramIE(InfoExtractor):
-    def _extract_nodes(self, nodes: Sequence[Any],
-                       b: bool) -> Sequence[Mapping[str, Any]]:
-        ...
+    ...
