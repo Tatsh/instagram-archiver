22ca9c63c8c9c95d7a18ec85c7565be5d31d3033
Andrew Udvare <audvare@gmail.com>
2022-06-07 17:51:37 -0400

    instagram_archiver/constants: format

--- instagram_archiver/constants.py
+++ instagram_archiver/constants.py
@@ -3,8 +3,9 @@ from typing import Final, Mapping
 __all__ = ('EXTRACT_VIDEO_INFO_JS', 'EXTRACT_XIGSHAREDDATA_JS',
            'SHARED_HEADERS', 'USER_AGENT')
 
-USER_AGENT: Final[str] = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                          '(KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
+USER_AGENT: Final[str] = ('Mozilla/5.0 (X11; Linux x86_64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/103.0.0.0 Safari/537.36')
 SHARED_HEADERS: Final[Mapping[str, str]] = {
     'accept':
     ('text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,'
