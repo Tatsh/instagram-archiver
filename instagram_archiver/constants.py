a1529ed9b38a725b78f57a9468f95b61978115ec
Andrew Udvare <audvare@gmail.com>
2023-04-01 21:41:46 -0400

    instagram_archiver/constants: update user agent, remove unused

--- instagram_archiver/constants.py
+++ instagram_archiver/constants.py
@@ -1,11 +1,10 @@
 from typing import Final, Mapping
 
-__all__ = ('EXTRACT_VIDEO_INFO_JS', 'EXTRACT_XIGSHAREDDATA_JS',
-           'SHARED_HEADERS', 'USER_AGENT')
+__all__ = ('SHARED_HEADERS', 'USER_AGENT')
 
 USER_AGENT: Final[str] = ('Mozilla/5.0 (X11; Linux x86_64) '
                           'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/103.0.0.0 Safari/537.36')
+                          'Chrome/112.0.0.0 Safari/537.36')
 SHARED_HEADERS: Final[Mapping[str, str]] = {
     'accept':
     ('text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,'
@@ -32,29 +31,3 @@ SHARED_HEADERS: Final[Mapping[str, str]] = {
     'x-ig-app-id':
     '936619743392459'
 }
-EXTRACT_XIGSHAREDDATA_JS: Final[str] = '''const qpl_inl = () => {};
-class ServerJS {
-  handleWithCustomApplyEach(x, z) {
-    for (x of z.define) {
-      if (x[0] === 'XIGSharedData') {
-        console.log(JSON.stringify(x[2].native, null, 2));
-      }
-    }
-  }
-}
-const requireLazy = (a, b) => {
-  b({ runWithPriority: (_, cb) => cb() }, ServerJS, () => {});
-};'''
-EXTRACT_VIDEO_INFO_JS: Final[str] = '''const qpl_inl = () => {};
-class ServerJS {
-  handleWithCustomApplyEach(x, z) {
-    for (x of z.require) {
-      if (x[0] === 'CometPlatformRootClient') {
-        console.log(JSON.stringify(x[3][3], null, 2));
-      }
-    }
-  }
-}
-const requireLazy = (a, b) => {
-  b({ runWithPriority: (_, cb) => cb() }, ServerJS, () => {});
-};'''
